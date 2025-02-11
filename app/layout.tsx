f50189d3e29e8b802b8d9d41ac9061558f593175
JafrulSadik <jafrul.sadik4@gmail.com>
2025-02-12 00:51:54 +0600

    Add Navbar and Footer components to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/common/Footer";
+import Navbar from "@/components/common/Navbar";
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
