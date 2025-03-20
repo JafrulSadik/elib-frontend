ad19c4daa4553491787463a3da58561726d42fca
JafrulSadik <jafrul.sadik4@gmail.com>
2025-03-21 01:51:50 +0600

    Refactor Footer component: improve layout, enhance responsiveness, and update styles

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -12,29 +12,33 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-b from-[#2B1810] to-[#1a0f0a] pt-16">
-      <div className="container mx-auto px-6 py-6">
+    <footer className="bg-gradient-to-b from-[#2B1810] to-[#1a0f0a]">
+      <div className="container w-[90%] md:w-full">
         {/* Top Section with Logo and Newsletter */}
-        <div className="flex flex-col lg:flex-row justify-between items-center mb-12 border-b border-t border-[#C5A572]/10 py-12">
-          <div className="flex items-center mb-8 lg:mb-0">
-            <BookOpen className="w-10 h-10 text-[#C5A572]" />
-            <div className="ml-4">
-              <h3 className="text-2xl font-bold text-[#C5A572]">E-LIBRARY</h3>
-              <p className="text-gray-400">Your Gateway to Knowledge</p>
+        <div className="flex flex-col lg:flex-row justify-between md:items-center mb-12  md:py-12">
+          <div className="flex items-center mb-4 md:mb-8 lg:mb-0">
+            <BookOpen className="size-8 md:size-10 text-textPrimary" />
+            <div className="ml-2 md:ml-4">
+              <h3 className="text-lg md:text-2xl font-bold text-textPrimary">
+                E-LIBRARY
+              </h3>
+              <p className="text-sm md:text-base text-gray-400">
+                Your Gateway to Knowledge
+              </p>
             </div>
           </div>
 
           <div className="w-full lg:w-1/3">
-            <h4 className="text-[#C5A572] font-semibold mb-4 text-center lg:text-left">
+            <h4 className="text-textPrimary font-semibold my-2 md:mb-4 md:text-center lg:text-left text-sm md:text-base">
               Subscribe to Our Newsletter
             </h4>
-            <div className="flex">
+            <div className="flex text-sm md:text-base">
               <input
                 type="email"
                 placeholder="Enter your email"
-                className="flex-1 px-4 py-3 bg-[#3D261C] text-white rounded-l-lg focus:outline-none focus:ring-2 focus:ring-[#C5A572]/50"
+                className="flex-1 px-2 py-2 md:px-4 md:py-3 bg-[#3D261C] text-white rounded-l-md md:rounded-l-lg focus:outline-none focus:ring-2 focus:ring-[#C5A572]/50"
               />
-              <button className="px-6 py-3 bg-[#C5A572] text-[#2B1810] rounded-r-lg hover:bg-[#D4B684] transition duration-300 font-semibold">
+              <button className="text-xs md:text-base px-3 md:px-6 md:py-3 bg-[#C5A572] text-[#2B1810]  rounded-r-md md:rounded-r-lg hover:bg-[#D4B684] transition duration-300 font-semibold">
                 Subscribe
               </button>
             </div>
@@ -42,51 +46,51 @@ export default function Footer() {
         </div>
 
         {/* Main Footer Content */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 mb-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-12 mb-12">
           {/* About Section */}
-          <div>
-            <h4 className="text-[#C5A572] font-semibold mb-6 text-lg">
+          <div className="text-sm md:text-base">
+            <h4 className="text-textPrimary font-semibold mb-2 md:mb-6 text-base md:text-lg">
               About Us
             </h4>
-            <p className="text-gray-400 mb-6 leading-relaxed">
+            <p className="text-xs md:text-sm text-gray-400 mb-2 md:mb-6 leading-relaxed">
               Discover millions of eBooks, audiobooks, and more at your
               fingertips. Join our community of book lovers and explore endless
               possibilities.
             </p>
-            <div className="flex space-x-4">
-              <a
+            <div className="flex space-x-2 md:space-x-4">
+              <Link
                 href="#"
-                className="w-10 h-10 rounded-full bg-[#3D261C] flex items-center justify-center text-[#C5A572] hover:bg-[#C5A572] hover:text-[#2B1810] transition duration-300"
+                className="size-7 md:size-10 rounded-full bg-[#3D261C] flex items-center justify-center text-textPrimary hover:bg-[#C5A572] hover:text-[#2B1810] transition duration-300"
               >
-                <Facebook className="w-5 h-5" />
-              </a>
-              <a
+                <Facebook className="size-3 md:size-5" />
+              </Link>
+              <Link
                 href="#"
-                className="w-10 h-10 rounded-full bg-[#3D261C] flex items-center justify-center text-[#C5A572] hover:bg-[#C5A572] hover:text-[#2B1810] transition duration-300"
+                className="size-7 md:size-10 rounded-full bg-[#3D261C] flex items-center justify-center text-textPrimary hover:bg-[#C5A572] hover:text-[#2B1810] transition duration-300"
               >
-                <Twitter className="w-5 h-5" />
-              </a>
-              <a
+                <Twitter className="size-3 md:size-5" />
+              </Link>
+              <Link
                 href="#"
-                className="w-10 h-10 rounded-full bg-[#3D261C] flex items-center justify-center text-[#C5A572] hover:bg-[#C5A572] hover:text-[#2B1810] transition duration-300"
+                className="size-7 md:size-10 rounded-full bg-[#3D261C] flex items-center justify-center text-textPrimary hover:bg-[#C5A572] hover:text-[#2B1810] transition duration-300"
               >
-                <Instagram className="w-5 h-5" />
-              </a>
-              <a
+                <Instagram className="size-3 md:size-5" />
+              </Link>
+              <Link
                 href="#"
-                className="w-10 h-10 rounded-full bg-[#3D261C] flex items-center justify-center text-[#C5A572] hover:bg-[#C5A572] hover:text-[#2B1810] transition duration-300"
+                className="size-7 md:size-10 rounded-full bg-[#3D261C] flex items-center justify-center text-textPrimary hover:bg-[#C5A572] hover:text-[#2B1810] transition duration-300"
               >
-                <Linkedin className="w-5 h-5" />
-              </a>
+                <Linkedin className="size-3 md:size-5" />
+              </Link>
             </div>
           </div>
 
           {/* Quick Links */}
           <div>
-            <h4 className="text-[#C5A572] font-semibold mb-6 text-lg">
+            <h4 className=" text-textPrimary font-semibold mb-2 md:mb-6 text-base md:text-lg">
               Quick Links
             </h4>
-            <ul className="space-y-4">
+            <ul className="text-sm md:text-base space-y-2 md:space-y-4 list-none">
               {[
                 "Home",
                 "About Us",
@@ -94,12 +98,11 @@ export default function Footer() {
                 "Latest Releases",
                 "Contact Us",
               ].map((item) => (
-                <li key={item}>
+                <li key={item} className="">
                   <Link
                     href="/"
-                    className="text-gray-400 hover:text-[#C5A572] transition duration-300 flex items-center"
+                    className="text-gray-400 hover:text-textPrimary transition duration-300 flex items-center"
                   >
-                    <span className="w-2 h-2 bg-[#C5A572] rounded-full mr-3"></span>
                     {item}
                   </Link>
                 </li>
@@ -109,10 +112,10 @@ export default function Footer() {
 
           {/* Support */}
           <div>
-            <h4 className="text-[#C5A572] font-semibold mb-6 text-lg">
+            <h4 className="text-textPrimary font-semibold mb-2 md:mb-6 text-base md:text-lg">
               Support
             </h4>
-            <ul className="space-y-4">
+            <ul className="space-y-2 md:space-y-4 text-sm md:text-base">
               {[
                 "FAQ",
                 "Help Center",
@@ -123,9 +126,8 @@ export default function Footer() {
                 <li key={item}>
                   <Link
                     href="/"
-                    className="text-gray-400 hover:text-[#C5A572] transition duration-300 flex items-center"
+                    className="text-gray-400 hover:text-textPrimary transition duration-300 flex items-center"
                   >
-                    <span className="w-2 h-2 bg-[#C5A572] rounded-full mr-3"></span>
                     {item}
                   </Link>
                 </li>
@@ -135,22 +137,22 @@ export default function Footer() {
 
           {/* Contact Info */}
           <div>
-            <h4 className="text-[#C5A572] font-semibold mb-6 text-lg">
+            <h4 className="text-textPrimary font-semibold mb-2 md:mb-6 text-base md:text-lg">
               Contact Us
             </h4>
-            <div className="space-y-4">
+            <div className="space-y-2 md:space-y-4 text-sm md:text-base">
               <div className="flex items-start">
-                <MapPin className="w-5 h-5 text-[#C5A572] mt-1 mr-3" />
+                <MapPin className="size-4 md:size-5 text-textPrimary mt-1 mr-3" />
                 <p className="text-gray-400">
                   123 Library Street, Book City, BC 12345
                 </p>
               </div>
               <div className="flex items-center">
-                <Phone className="w-5 h-5 text-[#C5A572] mr-3" />
+                <Phone className="size-4 md:size-5 text-textPrimary mr-3" />
                 <p className="text-gray-400">+1 (555) 123-4567</p>
               </div>
               <div className="flex items-center">
-                <Mail className="w-5 h-5 text-[#C5A572] mr-3" />
+                <Mail className="size-4 md:size-5 text-textPrimary mr-3" />
                 <p className="text-gray-400">support@elibrary.com</p>
               </div>
             </div>
@@ -158,7 +160,7 @@ export default function Footer() {
         </div>
 
         {/* Bottom Bar */}
-        <div className="border-t border-[#C5A572]/10 pt-6">
+        <div className="border-t border-[#C5A572]/10 py-6 text-sm md:text-base">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm mb-4 md:mb-0">
               © {new Date().getFullYear()} E-Library. All rights reserved.
@@ -166,19 +168,19 @@ export default function Footer() {
             <div className="flex space-x-6">
               <Link
                 href="/"
-                className="text-gray-400 hover:text-[#C5A572] text-sm transition duration-300"
+                className="text-gray-400 hover:text-textPrimary text-sm transition duration-300"
               >
                 Terms
               </Link>
               <Link
                 href="/"
-                className="text-gray-400 hover:text-[#C5A572] text-sm transition duration-300"
+                className="text-gray-400 hover:text-textPrimary text-sm transition duration-300"
               >
                 Privacy
               </Link>
               <Link
                 href="/"
-                className="text-gray-400 hover:text-[#C5A572] text-sm transition duration-300"
+                className="text-gray-400 hover:text-textPrimary text-sm transition duration-300"
               >
                 Cookies
               </Link>
