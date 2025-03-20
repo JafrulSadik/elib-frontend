b060ce6b47eb1c50795eb56988082267dc4d6063
JafrulSadik <jafrul.sadik4@gmail.com>
2025-03-21 01:56:46 +0600

    Fetch popular books data using SWR, enhance layout, and add loading skeletons

--- components/home/PopularBooks.tsx
+++ components/home/PopularBooks.tsx
@@ -1,128 +1,82 @@
+"use client";
+import { fetcher } from "@/lib/fetcher";
+import { ApiResponse } from "@/types/ApiResponse";
+import { Book, PaginationType } from "@/types/Book";
+import useEmblaCarousel from "embla-carousel-react";
+import useSWR from "swr";
+import PopularBookCardSkeleton from "../skeleton/PopularBookCardSkeleton";
 import PopularBookCard from "./PopularBookCard";
 
-const books = [
-  {
-    id: 1,
-    title: "The Silent Patient",
-    author: "Alex Michaelides",
-    cover:
-      "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.8,
-    reviews: 2547,
-  },
-  {
-    id: 2,
-    title: "Where the Crawdads Sing",
-    author: "Delia Owens",
-    cover:
-      "https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.7,
-    reviews: 1893,
-  },
-  {
-    id: 3,
-    title: "The Thursday Murder Club",
-    author: "Richard Osman",
-    cover:
-      "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.6,
-    reviews: 1245,
-  },
-  {
-    id: 4,
-    title: "The Silent Patient",
-    author: "Alex Michaelides",
-    cover:
-      "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.8,
-    reviews: 2547,
-  },
-  {
-    id: 5,
-    title: "Where the Crawdads Sing",
-    author: "Delia Owens",
-    cover:
-      "https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.7,
-    reviews: 1893,
-  },
-  {
-    id: 6,
-    title: "The Thursday Murder Club",
-    author: "Richard Osman",
-    cover:
-      "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.6,
-    reviews: 1245,
-  },
+const PopularBooks = () => {
+  const {
+    data: bookResponse,
+    error,
+    isLoading,
+  } = useSWR<ApiResponse<Book[], PaginationType>>(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/books/popular?limit=10`,
+    (url: string) => fetcher<Book[], PaginationType>(url) // Passing the type
+  );
 
-  {
-    id: 7,
-    title: "The Thursday Murder Club",
-    author: "Richard Osman",
-    cover:
-      "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.6,
-    reviews: 1245,
-  },
-  {
-    id: 8,
-    title: "The Silent Patient",
-    author: "Alex Michaelides",
-    cover:
-      "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.8,
-    reviews: 2547,
-  },
-  {
-    id: 9,
-    title: "Where the Crawdads Sing",
-    author: "Delia Owens",
-    cover:
-      "https://images.unsplash.com/photo-1512820790803-83ca734da794?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.7,
-    reviews: 1893,
-  },
-  {
-    id: 10,
-    title: "The Thursday Murder Club",
-    author: "Richard Osman",
-    cover:
-      "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-    rating: 4.6,
-    reviews: 1245,
-  },
-];
+  const books = bookResponse?.data ?? [];
 
-const PopularBooks = async () => {
-  // const response = (await getPopularBooks()) as ApiResponse<
-  //   Book[],
-  //   PaginationType
-  // >;
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    align: "center",
+    dragFree: false,
+    containScroll: "trimSnaps",
+  });
 
-  // const books = response.code === 200 ? response?.data ?? [] : [];
   return (
-    <section className="my-12">
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center my-6">
-          <h2 className="text-2xl font-bold text-[#C5A572]">Popular Books</h2>
-          <button className="text-[#C5A572] hover:text-[#D4B684]">
+    <section className="py-8 md:py-14">
+      <div className="container w-[90%] md:w-full">
+        <div className="flex justify-between items-center md:pb-8">
+          <h2 className="text-textPrimary text-lg  md:text-2xl font-bold">
+            Popular Books
+          </h2>
+          <button className="text-[#C5A572] hover:text-[#D4B684] text-sm">
             Show More
           </button>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 my-10 lg:grid-cols-5 lg:grid-rows-1 gap-7">
-          {books &&
-            books?.map((book, i) => (
-              <PopularBookCard
-                key={book?.id}
-                id={book.id}
-                cover={book?.cover}
-                title={book?.title}
-                author={book?.author}
-                price={"500"}
-                rating={4.6 + i * 0.1}
-              />
-            ))}
+
+        <div className="hidden md:grid md:grid-cols-3  lg:grid-cols-5 lg:grid-rows-1 gap-6 lg:gap-7">
+          {!isLoading
+            ? books?.map((book, i) => (
+                <PopularBookCard
+                  key={i}
+                  id={book._id}
+                  cover={book?.cover}
+                  title={book?.title}
+                  author={book?.author?.name}
+                  price={"500"}
+                  rating={4.6 + i * 0.1}
+                  ranking={i < 9 ? "0" + `${i + 1}` : i + 1}
+                />
+              ))
+            : Array(5)
+                .fill(0)
+                .map((_, i) => <PopularBookCardSkeleton key={i} />)}
+        </div>
+
+        <div className="md:hidden relative my-4 md:my-5 flex items-center">
+          <div className="overflow-hidden" ref={emblaRef}>
+            <div className="flex gap-4">
+              {!isLoading
+                ? books?.map((book, i) => (
+                    <PopularBookCard
+                      key={i}
+                      id={book._id}
+                      cover={book?.cover}
+                      title={book?.title}
+                      author={book?.author?.name}
+                      price={"500"}
+                      rating={4.6 + i * 0.1}
+                      ranking={i + 1}
+                    />
+                  ))
+                : Array(5)
+                    .fill(0)
+                    .map((_, i) => <PopularBookCardSkeleton key={i} />)}
+            </div>
+          </div>
         </div>
       </div>
     </section>
