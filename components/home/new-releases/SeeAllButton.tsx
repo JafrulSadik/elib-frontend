ca1f523348b02083e1b984e18cfbc1fec540816d
JafrulSadik <jafrul.sadik4@gmail.com>
2025-03-26 02:59:02 +0600

    Add SeeAllButton and SeeAllNewReleases components for navigation to new releases

---
+++ components/home/new-releases/SeeAllButton.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const SeeAllButton = () => {
+  return (
+    <Link
+      href="/new-releases"
+      className="relative flex items-center justify-center px-2 py-2 md:px-6 md:py-3 rounded-md bg-bgSecondary font-semibold text-gray-300 overflow-hidden group"
+    >
+      {/* Animated Background */}
+      <span className="absolute  inset-0 bg-textPrimary transform scale-x-0 origin-left transition-transform duration-500 group-hover:scale-x-100"></span>
+
+      <span className="relative text-textPrimary z-10 tracking-wide transition-colors duration-500 text-xs md:text-base  group-hover:text-bgPrimary">
+        See all new releases
+      </span>
+    </Link>
+  );
+};
+
+export default SeeAllButton;
