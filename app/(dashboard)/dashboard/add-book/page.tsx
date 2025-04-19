6d84d132e4f08697a19edf2fada58a38e9169e83
JafrulSadik <jafrul.sadik4@gmail.com>
2025-04-19 23:58:17 +0600

    Add dashboard pages and layout: implement AddBookPage, MyBooksPage, ProfilePage, and DashboardLayout with necessary components

--- app/(dashboard)/dashboard/add-book/page.tsx
+++ app/(dashboard)/dashboard/add-book/page.tsx
@@ -1,6 +1,9 @@
 import AddBookForm from "@/components/add-book/AddBookForm";
+import { getGenres } from "@/lib/fetchData";
 
-const AddBookPage = () => {
+const AddBookPage = async () => {
+  const { data: genres, message, code } = await getGenres();
+
   return (
     <div className="my-12 w-full">
       <h1 className="text-3xl font-bold text-[#C5A572] mb-2 text-center">
@@ -8,7 +11,7 @@ const AddBookPage = () => {
       </h1>
 
       <div className="max-w-2xl mx-auto rounded-lg p-6">
-        <AddBookForm />
+        <AddBookForm genres={genres || []} />
       </div>
     </div>
   );
