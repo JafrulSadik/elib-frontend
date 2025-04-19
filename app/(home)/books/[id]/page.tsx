68bf0d728d554bb10a6bec7a30eef61afb9c7a03
JafrulSadik <jafrul.sadik4@gmail.com>
2025-04-19 23:58:46 +0600

    Refactor layout components: simplify RootLayout and HomepageLayout, and create a new BooksPage component

--- app/(home)/books/[id]/page.tsx
+++ app/(home)/books/[id]/page.tsx
@@ -8,10 +8,10 @@ export default async function BookPage({ params }: { params: { id: string } }) {
 
   return (
     <div className="relative  min-h-screen bg-gradient-to-b from-[#2B1810] to-[#1a0f0a]">
-      <div className="container mx-auto px-4 py-12">
+      <div className="container mx-auto md:px-4 md:py-12">
         <BookHeader bookId={id} />
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <div className="lg:col-span-2 space-y-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 md:gap-8">
+          <div className="lg:col-span-2 md:space-y-8">
             <BookDescription bookId={id} />
             <ReviewSection />
           </div>
---
+++ app/(home)/books/page.tsx
@@ -0,0 +1,7 @@
+import BooksSearchPage from "@/components/books/BooksSearchPage";
+
+const BooksPage = () => {
+  return <BooksSearchPage />;
+};
+
+export default BooksPage;
