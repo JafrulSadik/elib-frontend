61c08381bc95bc3baf5f0845db016066493ffda9
JafrulSadik <jafrul.sadik4@gmail.com>
2025-04-20 00:04:15 +0600

    Add getGenres function to fetch genre data from API

--- lib/fetchData.ts
+++ lib/fetchData.ts
@@ -1,6 +1,6 @@
 import { ApiErrorResopnse, ApiResponse } from "@/types/ApiResponse";
 import { Book, PaginationType } from "@/types/Book";
-import { GenreWithBooks } from "@/types/Genre";
+import { Genre, GenreWithBooks } from "@/types/Genre";
 import { auth } from "./auth/auth";
 
 export const fetchData = async (endpoint: string) => {
@@ -88,6 +88,25 @@ export const getGenresWithBook = async (): Promise<
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/genres/genres-with-books`
     );
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch books: ${res.statusText}`);
+    }
+
+    return await res.json();
+  } catch (err) {
+    const error = err as Error;
+    return {
+      code: 500,
+      message: error.message,
+      data: [],
+    };
+  }
+};
+
+export const getGenres = async (): Promise<ApiResponse<Genre[]>> => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/genres`);
 
     if (!res.ok) {
       throw new Error(`Failed to fetch books: ${res.statusText}`);
