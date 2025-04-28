a37ef3171166b69fdb265cb36242a3a27af2ad0c
JafrulSadik <jafrul.sadik4@gmail.com>
2025-04-28 14:42:01 +0600

    feat: add bookId prop to MiniCard in TrendingBookSlider for improved data handling

--- components/home/trending-books/TrendingBookSlider.tsx
+++ components/home/trending-books/TrendingBookSlider.tsx
@@ -38,6 +38,7 @@ const TrendingBookSlider = ({ books }: { books: Book[] }) => {
             {books.map((book, i) => (
               <MiniCard
                 key={book?._id}
+                bookId={book?._id}
                 title={book?.title}
                 cover={book?.cover}
               />
